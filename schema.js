787b611f7b9c9b4d10a2db26982d779815cc524f
karan aghav <aghavkaran164@gmail.com>
2025-07-31 16:44:11 +0530

    rewrite schemas for listing and review

--- schema.js
+++ schema.js
@@ -1,21 +1,21 @@
-const JOI=require("joi");
+const JOI = require("joi");
 
-module.exports= listingSchema=JOI.object({
-    listing:JOI.object({
-         title:JOI.string().required(),
-         description:JOI.string().required(),
-         location:JOI.string().required(),
-         price:JOI.number().required().min(0),
-         country:JOI.string().required(),
-         image:JOI.string().allow(null, "")
-    }).required()
-         
-});
-     
-
-module.exports.reviewSchema=JOI.object({
-    review:JOI.object({
-        rating:JOI.number().required().min(1).max(5),
-        comment:JOI.string().required()
+const listingSchema = JOI.object({
+    listing: JOI.object({
+        title: JOI.string().required(),
+        description: JOI.string().required(),
+        location: JOI.string().required(),
+        price: JOI.number().required().min(0),
+        country: JOI.string().required(),
+        image: JOI.string().allow(null, "")
     }).required()
 });
+
+const reviewSchema = JOI.object({
+    review: JOI.object({
+        rating: JOI.number().required().min(1).max(5),
+        comment: JOI.string().required()
+    }).required()
+});
+
+module.exports = { listingSchema, reviewSchema };
